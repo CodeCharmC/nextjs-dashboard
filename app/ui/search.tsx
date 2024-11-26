7d7e47c6a064ad37ac3e2f599f25c21bc3001444
CodeCharmC <moni.akhtar1066@gmail.com>
2024-11-26 20:21:52 +0600

    text out Searching

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,6 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
 
   function handleSearch(term: string) {
+    console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
     if (term) {
